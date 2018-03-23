90824eebf3657b4707d4e74823e532e6e6f97d1c
Hieu <tranhieu956230@gmail.com>
2018-03-23 18:02:48 +0700

    second update

--- index.js
+++ index.js
@@ -2,37 +2,67 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css"
 
-var store = ["1","2","4","5","6"];
+var store = [];
 
+window.onbeforeunload = function () {
+    sessionStorage.setItem("1", JSON.stringify(store));
+    return undefined;
+};
+window.onload = function () {
+    store = JSON.parse(sessionStorage["1"]);
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+class Span extends React.Component {
+    close(e) {
+        e.stopPropagation();
+
+        this.props.close(this.props.ind);
+    }
+    render() {
+        return (
+            <span onClick={this.close.bind(this)}>x</span>
+        )
+    }
+}
 class App extends React.Component {
     constructor() {
         super();
-        this.state = { stor: store.map((tex,i) => <li>{tex}<span onClick={this.close.bind(this,i)}>x</span></li>) };
+        this.state = { stor: store.map((obj, i) => <li onClick={this.handleComplete.bind(this, i)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={i} close={this.close.bind(this, i)} /></li>) };
+    }
+    handleComplete(i, e) {
+        e.target.classList.toggle("completed");
+        if (store[i].completed == 0) store[i].completed = 1;
+        else store[i].completed = 0;
+        this.setState({ stor: store.map((obj, t) => <li onClick={this.handleComplete.bind(this, t)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={t} close={this.close.bind(this, t)} /></li>) });
+
     }
     handleClick() {
-        store.push(this.text);
-        this.setState ({ stor: store.map((tex,i) => <li>{tex}<span onClick={this.close.bind(this,i)}>x</span></li>)}) ;
+        store.push({ tex: this.text, completed: 0 });
+        this.setState({ stor: store.map((obj, i) => <li onClick={this.handleComplete.bind(this, i)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={i} close={this.close.bind(this, i)} /></li>) });
+
     }
     handleInput(e) {
         this.text = e.target.value;
     }
-    close(i){
-        store.splice(i,1);
-        console.log(i);
-        this.setState ({ stor: store.map((tex,i) => <li>{tex}<span onClick={this.close.bind(this,i)}>x</span></li>)}) ;
+    close(i) {
+        store.splice(i, 1);
+      
+        this.setState({ stor: store.map((obj, t) => <li onClick={this.handleComplete.bind(this, t)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={t} close={this.close.bind(this, t)} /></li>) });
+      
     }
     render() {
         return (
             <div id="main">
                 <div id="note">
                     <header>
-                      <h1>Note</h1>
+                        <h1>Note</h1>
                     </header>
                     <p>
-                        - ReactJS: Done<br/>
-                        - Add and Remove: Done <br/>
-                        - Completed: Coming soon...<br/>
-                        - Save On Page Load: Coming soon...<br/>
+                        - ReactJS: Done<br />
+                        - Add and Remove: Done <br />
+                        - Completed: Done<br />
+                        - Save On Page Load: Done
                     </p>
                 </div>
                 <div id="head">
@@ -52,7 +82,7 @@ class App extends React.Component {
 
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
 
 
 

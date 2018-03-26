69de14f254399098ffba98544284d6ab508e11ec
Hieu <tranhieu956230@gmail.com>
2018-03-26 16:41:11 +0700

    first commit

--- index.js
+++ index.js
@@ -1,93 +1,121 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style.css"
+import "./style.css";
 
 var store = [];
 
-window.onbeforeunload = function () {
-    sessionStorage.setItem("1", JSON.stringify(store));
-    return undefined;
-};
-window.onload = function () {
-    store = JSON.parse(sessionStorage["1"]);
-    ReactDOM.render(<App />, document.getElementById('root'));
-}
-
-class Span extends React.Component {
-    close(e) {
-        e.stopPropagation();
-
-        this.props.close(this.props.ind);
-    }
-    render() {
-        return (
-            <span onClick={this.close.bind(this)}>x</span>
-        )
-    }
-}
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = { stor: store.map((obj, i) => <li onClick={this.handleComplete.bind(this, i)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={i} close={this.close.bind(this, i)} /></li>) };
-    }
-    handleComplete(i, e) {
-        e.target.classList.toggle("completed");
-        if (store[i].completed == 0) store[i].completed = 1;
-        else store[i].completed = 0;
-        this.setState({ stor: store.map((obj, t) => <li onClick={this.handleComplete.bind(this, t)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={t} close={this.close.bind(this, t)} /></li>) });
-
-    }
-    handleClick() {
-        store.push({ tex: this.text, completed: 0 });
-        this.setState({ stor: store.map((obj, i) => <li onClick={this.handleComplete.bind(this, i)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={i} close={this.close.bind(this, i)} /></li>) });
-
-    }
-    handleInput(e) {
-        this.text = e.target.value;
-    }
-    close(i) {
-        store.splice(i, 1);
-      
-        this.setState({ stor: store.map((obj, t) => <li onClick={this.handleComplete.bind(this, t)} class={(obj.completed === 1) ? "completed" : ""} >{obj.tex}<Span ind={t} close={this.close.bind(this, t)} /></li>) });
-      
-    }
-    render() {
-        return (
-            <div id="main">
-                <div id="note">
-                    <header>
-                        <h1>Note</h1>
-                    </header>
-                    <p>
-                        - ReactJS: Done<br />
-                        - Add and Remove: Done <br />
-                        - Completed: Done<br />
-                        - Save On Page Load: Done
-                    </p>
-                </div>
-                <div id="head">
-                    <h1>My Todo List</h1>
-                    <form>
-                        <input type="text" placeholder="Title..." onChange={this.handleInput.bind(this)} />
-                        <button type="button" onClick={this.handleClick.bind(this)}>Add</button>
-                    </form>
-                </div>
-                <ul>
-                    {this.state.stor}
-                </ul>
-            </div>
-        )
-    }
-
-
+  constructor(){
+    super();
+    this.state={arr:store};
+  }
+  triggerUL(){
+  this.setState({arr:store});
+  }
+  render() {
+    return (
+      <div id="main">
+        <Head triggerUL={this.triggerUL.bind(this)}/>
+        <UL arr={this.state.arr}/>
+      </div>
+    )
+  }
 }
 
+class Head extends React.Component {
+  triggerUL(){
+    this.props.triggerUL();
+  }
+  render() {
+    return (
+      <div id="head">
+        <h1>My ToDo List</h1>
+        <Form triggerUL={this.triggerUL.bind(this)}/>
+      </div>
+    )
+  }
+}
+class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {text:''};
+  }
+  handleChange(e){
+   this.setState({text:e.target.value});
+   
+  }
+  addTodo(){
+    if(this.state.text!=='')store.push({text:this.state.text,completed:false});
+    this.setState({text:""});
+    this.props.triggerUL();
+  }
+  handleEnter(e){
+   
+   if(e.key==="Enter") {
+   if(this.state.text!=='')store.push({text:this.state.text,completed:false});
+   this.setState({text:""});
+   this.props.triggerUL();
+   }
 
+   return false;
+  }
+  render() {
+    return (
+      <form onSubmit={e=>e.preventDefault()}>
+        <input  type="text" onChange={this.handleChange.bind(this)} value={this.state.text} onKeyPress={this.handleEnter.bind(this)}/>
+        <button type="button" onClick={this.addTodo.bind(this)}>Add</button>
+      </form>
+    )
+  }
+}
+class UL extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={array:this.props.arr};
+  }
+  removeTodo(index){
+    store.splice(index,1);
+    this.setState({array:store});
+  }
+  handleComplete(index,e){
+   e.target.classList.toggle("completed");
+   if(store[index].completed===true) store[index].completed=false;
+   else store[index].completed=true;
+   this.setState({array:store});
 
+  }
+  render() {
+    var list = this.state.array;
+    list=list.map((obj,index)=>
+    <li key={index} onClick={this.handleComplete.bind(this,index)} className={obj.completed===true?"completed":""}>
+    {obj.text}
+    <Span close={this.removeTodo.bind(this)} id={index}/>
+    </li>
+    )
+    return (
+      <ul>{list}</ul>
+    )
+  }
+}
+class Span extends React.Component{
+  removeTodo(e){
+    e.stopPropagation();
+    this.props.close(e.target.id);
+  }
+  render(){
+    return(
+      <span onClick={this.removeTodo.bind(this)} id={this.props.id}>x</span>
+    )
+  }
+}
 
+window.onload= function(){
+store=JSON.parse(sessionStorage["todos"]);
+ReactDOM.render(<App />, document.getElementById('root'));
+   
+}
 
-
-
-
-
-
+window.onbeforeunload= function(){
+  sessionStorage.setItem("todos",JSON.stringify(store));
+  return undefined;
+}
